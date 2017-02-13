32f51f765b9d78376e20a596d3cfa77a4ff6a1bc
unknown <charly.philippe@free.fr>
2017-02-13 21:59:37 +0100

    removed PIDs other than luxfloat

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -40,9 +40,7 @@ typedef enum {
 } pidIndex_e;
 
 typedef enum {
-	PID_CONTROLLER_MW23 = 0,
-    PID_CONTROLLER_MWREWRITE,
-    PID_CONTROLLER_LUX_FLOAT,
+    PID_CONTROLLER_LUX_FLOAT = 0,
     PID_COUNT
 } pidControllerType_e;
 
